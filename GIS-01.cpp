ffd4b3fc48ef9d6212ef7e5a43e157907d8a02ae
saguila <saguila@ucm.es>
2015-02-19 23:45:21 +0100

    Create GIS-01.cpp

---
+++ GIS-01.cpp
@@ -0,0 +1,27 @@
+Enter file contents here
+#include<iostream>
+using namespace std;
+int calculaSerie(int base,int exponente);
+bool calculo();
+
+int main(){
+	while(calculo());
+	return 0;
+}
+
+bool calculo(){
+	int base,exponente;
+		cin >> base >> exponente;
+		if(!cin)return false;
+		cout << calculaSerie(base,exponente) << endl;
+		return true;
+}
+
+int calculaSerie(int base,int exponente){
+	int resultado = 1,acumulador = base;
+	for(int i = 0 ; i < exponente; i++){
+	resultado = (resultado  + acumulador) % 1000007;
+	acumulador = base * acumulador % 1000007;
+	}
+	return resultado;
+}
