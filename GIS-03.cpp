e52c7e6f3c4a32b52d13285d026b8f59c6af762b
saguila <saguila@ucm.es>
2015-02-19 23:50:32 +0100

    Create GIS-03.cpp

---
+++ GIS-03.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int calculo(int n, int x, int y);
+
+int main(){
+	const int M = 10000007;
+	int n, x, y;
+
+	while ((cin >> n), n != -1){
+		cin >> x >> y;
+		cout << calculo(n, x, y) % M<< endl;
+	}
+	return 0;
+}
+
+int calculo(int n, int x, int y){
+	if (n == 0) return x ;
+	else if (n == 1) return y;
+	else return calculo(n - 1, y, x + y);
+}
